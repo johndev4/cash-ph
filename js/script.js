c528b11f397ff9d53a3170df864b49d289e5cb1d
John Mistica <47683574+johndev4@users.noreply.github.com>
2020-07-05 20:59:05 +0800

    Update files

--- js/script.js
+++ js/script.js
@@ -1,33 +1,5 @@
 $(document).ajaxSuccess(function() {
-    const denomination_obj = document.getElementsByClassName('denom-val-holder')
-    const pieces_obj = document.getElementsByClassName('pieces');
-    const amount_obj = document.getElementsByClassName('amount');
-    
-    function update() {
-        var total = 0;
-
-        for (var i = 0; i < pieces_obj.length; i+=1){
-            var d = denomination_obj[i].textContent;
-            var p = pieces_obj[i].value;
-            var amt = parseFloat(d) * parseFloat(p);
-
-            amount_obj[i].textContent = amt.toLocaleString("en-US", {
-                style: "currency",
-                currency: "PHP"
-            });
-            total += amt;
-        }
-
-        $('#total').text(
-            total.toLocaleString("en-US", {
-                style: "currency",
-                currency: "PHP"
-            })
-        );
-    }
-
     update();
-
 
     //Pieces Object Event Listener
     $('.pieces').on('input', function() {
@@ -43,11 +15,15 @@ $(document).ajaxSuccess(function() {
 
 
     //Menu
-    $('#save').click(function() {
-        
+    $('#restore').click(function() {
+        restore();
     });
 
-    $('#print').click(function() {
+    $('#save').click(function() {
+        save();
+    });
+
+    $('#save-as-pdf').click(function() {
 
     });
 
@@ -62,3 +38,67 @@ $(document).ajaxSuccess(function() {
         update();
     });
 });
+
+
+const denomination_obj = document.getElementsByClassName('denom-val-holder')
+const pieces_obj = document.getElementsByClassName('pieces');
+const amount_obj = document.getElementsByClassName('amount');
+
+function update() {
+    var total = 0;
+
+    for (var i = 0; i < pieces_obj.length; i+=1){
+        var d = denomination_obj[i].textContent;
+        var p = pieces_obj[i].value;
+        var amt = parseFloat(d) * parseFloat(p);
+
+        amount_obj[i].textContent = amt.toLocaleString("en-US", {
+            style: "currency",
+            currency: "PHP"
+        });
+        total += amt;
+    }
+
+    $('#total').text(
+        total.toLocaleString("en-US", {
+            style: "currency",
+            currency: "PHP"
+        })
+    );
+}
+
+function restore() {
+    if (localStorage.length > 0){
+        var arr = JSON.parse(localStorage.getItem("piecesValues"));
+    } else{
+        showDialogBox("Information", "There is no saved data.", 3);
+    }
+}
+
+function save() {
+    var arr = [];
+    var notAllZero = false;
+    for (var i = 0; i < pieces_obj.length; i++){
+        arr.push(pieces_obj[i].value);
+        if (pieces_obj[i].value != 0){
+            notAllZero = true;
+        }
+    }
+    var str = JSON.stringify(arr);
+
+    if (notAllZero === true){
+        window.localStorage.setItem("piecesValues", str);
+        showDialogBox("Information", "Data succefully saved.", 3);
+    } else{
+        showDialogBox("Information", "All inputs are empty.", 3);
+    }
+}
+
+function showDialogBox(title, info, interval) {
+    $('#dialogboxLongTitle').text(title);
+    $('#dialogboxInfo').text(info);
+    $('#dialogbox').modal('show');
+    window.setInterval(function() {
+        $('#dialogbox').modal('hide');
+    }, interval*1000);
+}
